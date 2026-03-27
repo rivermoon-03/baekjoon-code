c89231a29a863b27001f8c34c94dccf9d67b2c33
rivremoon-03 <riverm00n@naver.com>
2026-03-27 21:08:16 +0900

    11723
    .

---
+++ 11723번 - 집합/Main.java
@@ -0,0 +1,31 @@
+//=====================================================================
+//   11723번:    집합                   
+//   @date:   2026-03-27              
+//   @link:   https://www.acmicpc.net/problem/11723  
+//   @Note:   폴더 내부에 있는 파일을 삭제하거나 변경하지 말아주세요.
+//   @Test:   코드를 작성 후 "BOJ: 테스트"통해서 테스트를 해보세요.
+//=====================================================================
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        Int lines = br.ReadLine();
+        System.out.println(lines);
+        
+
+        
+
+        
+        
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
