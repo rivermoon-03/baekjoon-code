f246e10f096b5b4332cfeb08ea66cbf73c5eebd3
rivermoon-03 <riverm00n@naver.com>
2026-03-30 22:38:01 +0900

    .
    .

--- 2026-03/10989번 - 수 정렬하기 3/app.py
+++ 2026-03/10989번 - 수 정렬하기 3/app.py
@@ -9,28 +9,26 @@
 import sys;
 
 input = sys.stdin.readline
+import sys
 
-# 어떤 정렬을 쓸 것인가? - 당연히 sort()는 안 씀.
-# 일단 속도가 안정적인 merge sort를 써 보려 한다.
+# 일단 sort로는 안됨. - 메모리 초과
+# 그럼 버블 정렬? -> 근데 얘도 메모리를 많이 먹는다.
 
-# merge sort
-# [1,2,3,4,5,6] -> [1, 2, 3] / [4, 5, 6] -> 
-# [1] / [2, 3] / [4] / [5, 6] ->
-# [1] [2] [3] [4] [5] [6]
+# 수는 1~10000까지 나옴. 개수는 10,000,000개. 즉 중복이 있다는 말.
+# 그럼 10000칸의 공간을 가진 배열을 만들고, 각 칸을 숫자가 나온 빈도 수로 생각하기.
+# -> Counting Sort.
 
+number = [0 for x in range(10001)] # number[1:10000]까지 사용할 것이다. 편하게 1부터 세기.
+n = int(input())
+for _ in range(n):
+    incoming = int(input())
+    number[incoming] += 1
 
-def merge_sort():
-    return
+for i in range(1, len(number)):
+    for _ in range(number[i]):
+        print(i)
 
-def merge():
-    return
-
-numbers = []
-
-cnt = int(input())
-for _ in range(cnt):
-    numbers.append(int(input().strip()))
-
-print(numbers)
-
+    
+    
+    
 
